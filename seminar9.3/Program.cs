6f8bfc7de7ebed38efceb94290a89b0583fdceb2
nikul1ch <nikulin@netkam.ru>
2022-09-22 16:24:50 +0300

    Программа, которая вычисляет функцию Аккермана с помощью рекурсии

--- seminar9.3/Program.cs
+++ seminar9.3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int AckermannFunction(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return AckermannFunction(n - 1, 1);
+    if (n > 0 && m > 0) return AckermannFunction(n - 1, AckermannFunction(n, m - 1));
+    return AckermannFunction(n, m);
+}
+
+int InputVariable(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = InputVariable("Введите первое неотрицательное натуральное чисел: ");
+int n = InputVariable("Введите второе неотрицательное натуральное чисел: ");
+
+Console.WriteLine($"Вычисление функции Аккермана = {AckermannFunction(m, n)}");
