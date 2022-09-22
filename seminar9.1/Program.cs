b949af89caf0c0fd3ead0d6eded7004c9d8a0e7c
nikul1ch <nikulin@netkam.ru>
2022-09-22 15:16:36 +0300

    first commit

---
+++ seminar9.1/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] CreateArray(int m, int n)
+{
+    if (m < 1 || n < 1)
+    {
+        Console.Write("Одно или оба числа не натуральные");
+        Environment.Exit(0);
+    }
+    int[] array = new int[n - m + 1];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        array[i] = m + i;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write($"|\t");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+    Console.Write("|");
+    Console.WriteLine();
+}
+
+int InputVariable(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = InputVariable("Введите нижнюю границу натуральных чисел: ");
+int n = InputVariable("Введите верхнюю границу натуральных чисел: ");
+
+PrintArray(CreateArray(m, n));
